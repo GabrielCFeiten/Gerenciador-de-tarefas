8d71e6ea77b19ac78c5407624eed4e7015a663a6
GabrielCFeiten <gabrielcamposfeiten@gmail.com>
2026-03-22 15:28:45 -0300

    Model add

---
+++ src/test/java/org/cf/gerenciadordetarefas/GerenciadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cf.gerenciadordetarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeTarefasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
